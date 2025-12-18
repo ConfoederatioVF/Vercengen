a3665fa4907b48a6434330ce3e20b0583f31636b
Confoederatio <htmlprogrammingbn@gmail.com>
2025-12-18 07:53:58 +0000

    Update ComponentNodeEditorDatatype.js

--- UF/js/vercengen/components/ComponentNodeEditor/ComponentNodeEditorDatatype.js
+++ UF/js/vercengen/components/ComponentNodeEditor/ComponentNodeEditorDatatype.js
@@ -1,5 +1,9 @@
 /**
  * Refer to <span color = "yellow">{@link ve.Component}</span> for methods or fields inherited from this Component's parent such as `.options.attributes` or `.element`.
+ * 
+ * ##### Constructor:
+ * - `arg1_options`: {@link Object}
+ *   - `.node_editor`: {@link ve.NodeEditor} - The node editor that this ve.NodeEditorDatatype is attached to.
  *
  * @augments ve.Component
  * @memberof ve.Component
@@ -13,5 +17,11 @@ ve.NodeEditorDatatype = class extends ve.Component {
 		let value = arg0_value;
 		let options = (arg1_options) ? arg1_options : {};
 			super(options);
+			
+		//Declare local instance variables
+		this.geometries = [];
+		this.options = options;
+		this.primary_geometry = new maptalks.GeometryCollection([]);
+		
 	}
 }
