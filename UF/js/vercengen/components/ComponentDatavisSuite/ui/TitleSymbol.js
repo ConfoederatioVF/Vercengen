7c56c0e3bc7f35854fff092d7fcb6c5ffa636e52
Confoederatio <htmlprogrammingbn@gmail.com>
2026-01-21 12:57:27 +0000

    started work on new title symbol

---
+++ UF/js/vercengen/components/ComponentDatavisSuite/ui/TitleSymbol.js
@@ -0,0 +1,56 @@
+ve.DatavisSuite.TitleSymbol = class extends ve.Component {
+	constructor (arg0_value, arg1_options) {
+		//Convert from parameters
+		let value = (arg0_value) ? arg0_value : {};
+		let options = (arg1_options) ? arg1_options : {};
+			super(options);
+			
+		//Initialise options
+		options.attributes = (options.attributes) ? options.attributes : {};
+			
+		//Declare local instance variables
+		this.element = document.createElement("div");
+			this.element.setAttribute("component", "ve-datavis-suite-title-symbol");
+			this.element.instance = this;
+			HTML.setAttributesObject(this.element, options.attributes);
+		this.value = {};
+		
+		this.from_binding_fire_silently = true;
+		this.v = value;
+		delete this.from_binding_fire_silently;
+	}
+	
+	get v () {
+		//Return statement
+		return this.value;
+	}
+	
+	set v (arg0_value) {
+		//Convert from parameters
+		let value = (arg0_value) ? arg0_value : {};
+		
+		//Parse value
+		this.element.innerHTML = "";
+		this.interface = new ve.Interface({
+			
+		}, { 
+			name: (this.options.name) ? this.options.name : "Title Symbol",
+			onuserchange: () => {
+				delete this.do_not_fire_to_binding;
+				this.fireToBinding();
+			}
+		});
+		this.interface.bind(this.element);
+		this.value = value;
+	}
+};
+
+//Functional binding
+
+/**
+ * @returns {ve.DatavisSuite.TitleSymbol}
+ */
+veDatavisSuiteTitleSymbol = function () {
+	//Return statement
+	return new ve.DatavisSuite.TitleSymbol(...arguments);
+};
