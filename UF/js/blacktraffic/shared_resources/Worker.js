b5738a25d417e3fd61f2b14a9d1ec87c9d9c4cce
Confoederatio <htmlprogrammingbn@gmail.com>
2026-02-26 19:29:12 +0000

    docs are now properly generated

--- UF/js/blacktraffic/shared_resources/Worker.js
+++ UF/js/blacktraffic/shared_resources/Worker.js
@@ -48,8 +48,6 @@ if (!global.Blacktraffic) global.Blacktraffic = {};
  * - `.workers_id_obj`: {@link Object}<{@link number}>
  * - `.workers_obj`: {@link Object}<{@link Array}<{@link Object}>>
  * 
- * @augments Blacktraffic
- * @memberof Blacktraffic
  * @type {Blacktraffic.Worker}
  */
 Blacktraffic.Worker = class {
@@ -115,8 +113,16 @@ Blacktraffic.Worker = class {
 		this.name = `${type} ${this.worker_id}`;
 		
 		//Declare consoles
+		/**
+		 * @type {log.Channel}
+		 * @memberof Blacktraffic.Worker
+		 */
 		this.console = (!log[options.log_channel]) ?
 			new log.Channel(options.log_channel) : log[`${options.log_channel}_instance`];
+		/**
+		 * @type {log.Channel}
+		 * @memberof Blacktraffic.Worker
+		 */
 		this.console_local = (!log[this.name]) ? 
 			new log.Channel(this.name) : log[`${this.name}_instance`];
 		
@@ -327,7 +333,6 @@ Blacktraffic.Worker = class {
 	 * Prints an error to both logging channels.
 	 * - Method of: {@link Blacktraffic.Worker}
 	 * 
-	 * @alias error
 	 * @memberof Blacktraffic.Worker
 	 * 
 	 * @param argn_arguments
@@ -338,7 +343,6 @@ Blacktraffic.Worker = class {
 	 * Prints a log to both logging channels.
 	 * - Method of: {@link Blacktraffic.Worker}
 	 *
-	 * @alias log
 	 * @memberof Blacktraffic.Worker
 	 * 
 	 * @param argn_arguments
@@ -349,7 +353,6 @@ Blacktraffic.Worker = class {
 	 * Prints a message type to both logging channels.
 	 * - Method of: {@link Blacktraffic.Worker}
 	 *
-	 * @alias print
 	 * @memberof Blacktraffic.Worker
 	 * 
 	 * @param {string} arg0_type - Either 'error'/'log'/'warn'.
@@ -369,7 +372,6 @@ Blacktraffic.Worker = class {
 	 * Removes the current worker.
 	 * - Method of: {@link Blacktraffic.Worker}
 	 *
-	 * @alias remove
 	 * @memberof Blacktraffic.Worker
 	 */
 	remove () {
@@ -500,7 +502,6 @@ Blacktraffic.Worker = class {
 	 * Logs a warning to both logging channels.
 	 * - Method of: {@link Blacktraffic.Worker}
 	 *
-	 * @alias warn
 	 * @memberof Blacktraffic.Worker
 	 * 
 	 * @param argn_arguments
